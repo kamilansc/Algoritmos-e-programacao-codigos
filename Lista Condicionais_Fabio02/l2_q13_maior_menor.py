5e1862c7c801d4118590bbcdc7e484f6de024ef5
Kamila Rocha <kamilarocha155@gmail.com>
2025-04-25 11:14:21 -0300

    lista condicionais - Fábio

--- Lista Condicionais_Fabio02/l2_q13_maior_menor.py
+++ Lista Condicionais_Fabio02/l2_q13_maior_menor.py
@@ -7,10 +7,9 @@ def main():
     num1 = obter_numero(label)
     num2 = obter_numero(label)
 
-    if num_eh_diferente(label, num1, num2, 0, 0, 0):
+    if num1 != num2:
         num3 = obter_numero(label)
-# num1 != num3 and num2 != num3
-        if num_eh_diferente(label, num1, num2, num3, 0 ,0):
+        if num1 != num3 and num2 != num3:
             num4 = obter_numero(label)
             if num1 != num4 and num2 != num4 and num3 != num4:      
                 num5 = obter_numero(label)
@@ -24,11 +23,36 @@ def main():
         print(f'\nO número "{num2}" já foi digitado anteriormente. Por favor digite todos os números novamente!')
         return main()
 
+    maior_numero = devolve_maior_numero(num1, num2, num3, num4, num5)
+    print(f'\nO MAIOR número é o "{maior_numero}".')
 
-def num_eh_diferente(label, num1, num2, num3, num4, num5):
-    if num1 != num2:
-        num3 = obter_numero(label)
-    return False
+    menor_numero = devolve_menor_numero(num1, num2, num3, num4, num5)
+    print(f'\nO MENOR número é o "{menor_numero}".')
 
+
+def devolve_maior_numero(num1, num2, num3, num4, num5):
+    if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
+        return num1
+    elif num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
+        return num2
+    elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
+        return num3
+    elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
+        return num4
+    elif num5 > num1 and num5 > num2 and num5 > num3 and num5 > num4:
+        return num5
+
+
+def devolve_menor_numero(num1, num2, num3, num4, num5):
+    if num1 < num2 and num1 < num3 and num1 < num4 and num1 < num5:
+        return num1
+    elif num2 < num1 and num2 < num3 and num2 < num4 and num2 < num5:
+        return num2
+    elif num3 < num1 and num3 < num2 and num3 < num4 and num3 < num5:
+        return num3
+    elif num4 < num1 and num4 < num2 and num4 < num3 and num4 < num5:
+        return num4
+    elif num5 < num1 and num5 < num2 and num5 < num3 and num5 < num4:
+        return num5
 
 main()
---
+++ Lista Condicionais_Fabio02/l2_q14_media.py
@@ -0,0 +1,2 @@
+# 14. Leia 5 (cinco) números inteiros, calcule a sua média e escreva os que são maiores que a média.
+
