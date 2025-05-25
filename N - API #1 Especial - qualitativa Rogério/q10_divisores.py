6ee3585a21b62088234db99a39818b81785f80aa
Kamila Rocha <kamilarocha155@gmail.com>
2025-05-25 20:45:38 -0300

    praticando

---
+++ N - API #1 Especial - qualitativa Rogério/q10_divisores.py
@@ -0,0 +1,22 @@
+# Início: 25/05 20h23
+# Fim:    25/05 20h42
+
+from q1_number_utils import obter_num_int_positivo, obter_num_int_min
+def main():
+    numA = obter_num_int_positivo('Número A: ')
+    numB = obter_num_int_min('Número B: ', 11 + numA)
+
+    num = numA
+    while num <= numB:
+        qtd_divisores = 0
+        divisor = 2
+
+        while divisor < num:
+            if num % divisor == 0:
+                qtd_divisores += 1
+            divisor += 1
+        print(f'{num}: ({qtd_divisores})')
+        num += 1
+
+
+main()
