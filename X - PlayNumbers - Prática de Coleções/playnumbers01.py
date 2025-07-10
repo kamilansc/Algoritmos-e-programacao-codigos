ac9390570176372a2174f5d1825127f05957ded8
Kamila Rocha <kamilarocha155@gmail.com>
2025-07-10 11:04:37 -0300

    Update playnumbers01.py

--- X - PlayNumbers - Prática de Coleções/playnumbers01.py
+++ X - PlayNumbers - Prática de Coleções/playnumbers01.py
@@ -2,8 +2,12 @@ import random
 from hello_map_filter_reduce import mapear
 
 def main():
-    menu = '''\n\tManipulação de Coleção (Vetores)
-    1 - Inicializar Vetor Numérico
+    menu = '''\n\t>>> Bem vindo ao PlayNumbers !!! <<<
+    1 - Inicializar Colecão Numérica
+    2 - Mostrar todos os valores
+    3 - Resetar coleção
+    4 - Mostrar quantidade de itens na coleção
+    
     0 - Sair
     Opção: '''
 
@@ -12,38 +16,45 @@ def main():
     while opcao != 0:
         if opcao == 1:
             tipo_colecao = int(input('''
->>> Inicializar Vetor Numérico
+>>> Escolha como você deseja gerar o conjunto de valores:
     1 - Gerar valores automaticamente
     2 - Informar valores
     3 - Carregar valores de um arquivo
     Opção: '''))
+            print('')
 
             if tipo_colecao == 1:
-                tamanho_colecao = int(input('Qtd valores: '))
-                minimo = int(input('Valor mínimo: '))
-                maximo = int(input('Valor máximo: '))
-
-                colecao = []
-                for item in range(1, tamanho_colecao+1):
-                    novo_valor = random.randint(minimo, maximo)
-                    colecao.append(novo_valor)
+                colecao = inicializar_vetor_com_valores_automaticos()
                 
-                opcao = int(input(menu))
-
             elif tipo_colecao == 2:
-                tamanho_colecao = int(input('Qtd valores: '))
-                minimo = int(input('Valor mínimo: '))
-                maximo = int(input('Valor máximo: '))
-
-                colecao = mapear(input('Digite os valores: ').split(), int)
-
-                opcao = int(input(menu))
+                colecao = inicializar_vetor_com_valores_do_usuario()
 
             elif tipo_colecao == 3:
-                nome_arquivo = input('Nome do arquivo: ')
+                nome_arquivo = input('Qual o nome do arquivo (adicione .txt ao final)? ')
                 colecao = carregar_valores_do_arquivo(nome_arquivo)
 
-                print(colecao)
+        if opcao == 2:
+            try:
+                print(f'\n\t--- Aqui estão os valores da sua coleção atual ---\n{' - '.join(str(n) for n in colecao)}')
+
+            except UnboundLocalError:
+                print('''
+    *** Não há nenhuma coleção de valores armazenada. 
+    *** Primeiramente, inicialize os valores na opção 1 do menu.''')
+
+        if opcao == 3:
+            try:
+                valor_padrao = int(input('Digite um valor para substituir todos os atuais valores: '))
+                colecao = resetar_valores_da_colecao(colecao, valor_padrao)
+                print('>>> Sua coleção foi resetada com sucesso!! <<<')
+
+            except UnboundLocalError:
+                print('''
+    *** Não há nenhuma coleção de valores armazenada. 
+    *** Primeiramente, inicialize os valores na opção 1 do menu.''')
+        
+
+        opcao = int(input(menu ))
 
 
 def carregar_valores_do_arquivo(nome_arquivo):
@@ -56,6 +67,52 @@ def carregar_valores_do_arquivo(nome_arquivo):
             colecao.append(item)
     
     return colecao
+
+
+def pedir_infos_sobre_vetor():
+    print('>>> Digite, a seguir, algumas informações sobre o vetor.')
+    tamanho_colecao = int(input('Qtd valores: '))
+    minimo = int(input('Valor mínimo: '))
+    maximo = int(input('Valor máximo: '))
+
+    return tamanho_colecao, minimo, maximo
+
+
+def inicializar_vetor_com_valores_automaticos():
+    colecao = []
+    tamanho_colecao, minimo, maximo = pedir_infos_sobre_vetor()
+
+    for item in range(1, tamanho_colecao+1):
+        novo_valor = random.randint(minimo, maximo)
+        colecao.append(novo_valor)
+
+    return colecao
+
+
+def inicializar_vetor_com_valores_do_usuario():
+    colecao = []
+    tamanho_colecao, minimo, maximo = pedir_infos_sobre_vetor()
+
+    for posicao in range(1, tamanho_colecao+1):
+        novo_valor = int(input(f'>>> Digite o {posicao}° valor: '))
+        
+        while novo_valor < minimo or novo_valor > maximo:
+            print('Valor inválido, por favor, tente novamente!\n')
+            novo_valor = int(input(f'>>> Digite o {posicao}° valor: '))
+   
+        colecao.append(novo_valor)
+
+    return colecao
+
+
+def resetar_valores_da_colecao(colecao, valor_padrao):
+    colecao = [valor_padrao for item in colecao]
+
+    return colecao
+    # nova_colecao = []
+    # for item in colecao:
+        # nova_colecao.append(valor_padrao)
+    # colecao = nova_colecao
 
 
 main()
